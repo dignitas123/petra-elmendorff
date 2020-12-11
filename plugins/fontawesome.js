e615cc610465620edce9a170696628e9ba395270
nic <niclashummel.fx@gmail.com>
2020-12-11 22:07:27 +0100

    fortawesome fix

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
 // This is important, we are going to let Nuxt.js worry about the CSS
