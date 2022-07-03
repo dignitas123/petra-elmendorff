62a21903fc492ad1b55a3e1f5a68f63b9fc9bc7a
dignitas123 <niclashumm@aol.com>
2022-07-03 14:12:27 +0200

    added prettier

--- store/index.js
+++ store/index.js
@@ -9,21 +9,21 @@ export const state = () => ({
 })
 
 export const getters = {
-  getSiteSettings (state) {
+  getSiteSettings(state) {
     return state.siteSettings
   },
-  getSessions (state) {
+  getSessions(state) {
     return state.sessions
   },
   // get Sessions split up by Dates (sessions with multiple dates get shallowcopied)
-  getDates (state) {
+  getDates(state) {
     let dates = []
     state.sessions.forEach(sessi => {
       // split up sessions by dates
       if (sessi.dates) {
-        sessi.dates.forEach(function(otherDate){
+        sessi.dates.forEach(function(otherDate) {
           //copy session object
-          let dummySession = Object.assign({}, sessi) 
+          let dummySession = Object.assign({}, sessi)
           // set old api stzle date
           dummySession.date = otherDate
           dummySession.originalSessionObj = sessi
@@ -31,30 +31,28 @@ export const getters = {
         })
       }
     })
-    dates.sort((a, b) =>
-      a.date.from > b.date.from ? 1 : -1
-    )
+    dates.sort((a, b) => (a.date.from > b.date.from ? 1 : -1))
     return dates
   },
-  getLanguage (state) {
+  getLanguage(state) {
     return state.language
   },
-  subheader (state) {
+  subheader(state) {
     return deepTranslate(state.siteSettings.subheader, state.language)
   },
-  currentSlug (state) {
+  currentSlug(state) {
     return state.currentSlug
   },
-  headermenue (state) {
+  headermenue(state) {
     return deepTranslate(state.siteSettings.headermenue, state.language)
   }
 }
 
 export const mutations = {
-  setSiteSettings (state, info) {
+  setSiteSettings(state, info) {
     state.siteSettings = info
   },
-  setSessions (state, info) {
+  setSessions(state, info) {
     state.sessions = info
     // state.sessions.forEach(session => {
     //   if (session.date) {
@@ -66,48 +64,43 @@ export const mutations = {
   // setProgram(state, program) {
   //   state.program = program
   // },
-  setLanguage (state, language) {
+  setLanguage(state, language) {
     state.language = language
   },
-  setCurrentSlug (state, slug) {
+  setCurrentSlug(state, slug) {
     state.currentSlug = slug
   }
 }
 
 export const actions = {
-  nuxtServerInit (vuexContext, {req, redirect, params, route, query}) {
-    let headerlang = req.headers['accept-language'] ? req.headers['accept-language'].split(',')[0].split('-')[0] : false
+  nuxtServerInit(vuexContext, { req }) {
+    let headerlang = req.headers['accept-language']
+      ? req.headers['accept-language'].split(',')[0].split('-')[0]
+      : false
 
-    let coockielang = this.$cookies.get("lang")
+    let coockielang = this.$cookies.get('lang')
 
-    if (coockielang){
+    if (coockielang) {
       vuexContext.commit('setLanguage', coockielang)
-    }
-    else if (headerlang == 'en') vuexContext.commit('setLanguage', 'en')
-
-    // console.log("request lang ", headerlang)
-    // console.log("coockie", this.$cookies.get("lang"))
-    // console.log("route ", query)
-
+    } else if (headerlang == 'en') vuexContext.commit('setLanguage', 'en')
   }
 }
 
-const deepTranslate = function (obj, lang, langDefault='de') {
+const deepTranslate = function(obj, lang, langDefault = 'de') {
   if (obj != null && Array.isArray(obj)) {
-    return obj.map(val => deepTranslate(val, lang, langDefault));
-  } 
-  else if (obj != null && typeof obj === 'object') {
-      if (obj.hasOwnProperty(lang) ) return obj[lang]
-      else if (obj.hasOwnProperty(langDefault) ) return obj[langDefault]
-      else {
-        var res = {};
-        for (var key in obj) {
-          if (obj.hasOwnProperty(key)) res[key] = deepTranslate(obj[key], lang, langDefault)
-        }
-        return res;
+    return obj.map(val => deepTranslate(val, lang, langDefault))
+  } else if (obj != null && typeof obj === 'object') {
+    if (obj.hasOwnProperty(lang)) return obj[lang]
+    else if (obj.hasOwnProperty(langDefault)) return obj[langDefault]
+    else {
+      var res = {}
+      for (var key in obj) {
+        if (obj.hasOwnProperty(key))
+          res[key] = deepTranslate(obj[key], lang, langDefault)
       }
-  } 
-  else {
-      return obj;
+      return res
+    }
+  } else {
+    return obj
   }
 }
